41267e7559f9f8f2128538cb5dfcfd761d59fa5a
nymalone <malone.nykolle@gmail.com>
2020-06-14 14:03:46 -0300

    dashboard done

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 
 import './styles.css';
 import logo from '../../assets/flat.png';
+import health from '../../assets/health.png';
+import exercise from '../../assets/exercise.png';
+import food from '../../assets/food.png';
 
-import { Navbar, Row, Col, Container, Card } from 'react-bootstrap';
-import { FaRegStar, FaRoad, FaBed, FaRoute } from 'react-icons/fa';
+import Footer from '../../components/Footer';
+import { Navbar, Row, Col, Container, Card, CardDeck } from 'react-bootstrap';
+import { FaRegStar, FaRoad, FaBed, FaRoute, FaMedal } from 'react-icons/fa';
 
 export default class Home extends Component {
   render() {
@@ -59,6 +63,82 @@ export default class Home extends Component {
             </Col>
           </Row>
         </Container>
+
+        <Container fluid id="ranking">
+          <Row>
+            <Col>
+              <br />
+              <h2>Ranking da Cristal</h2>
+              <p>
+                <FaMedal /> <b>Tonhão - </b> 10.000 pontos
+              </p>
+              <p>
+                <FaMedal /> <b>João - </b> 9.000 pontos
+              </p>
+              <p>
+                <FaMedal />
+                <b>Tião - </b> 8.000 pontos
+              </p>
+              <span>
+                VOCÊ <br />
+                3.000 pontos{' '}
+              </span>
+              <br />
+              <br />
+              <br />
+              <h4>Convide amigos para participar e ganhe pontos!</h4>
+              <button>CONVIDAR</button>
+            </Col>
+          </Row>
+        </Container>
+
+        <CardDeck className="tips mt-5 mb-5">
+          <Card>
+            <Card.Img variant="top" src={health} />
+            <Card.Body>
+              <a href="/">
+                {' '}
+                <h3>Dicas de Saúde</h3>{' '}
+              </a>
+              <Card.Text>
+                Tem alguma dúvida sobre seus remédios? Quer saber como melhorar
+                a sua saúde? Confira aqui dicas de especialista.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={exercise} />
+            <Card.Body>
+              <a href="/">
+                {' '}
+                <h3>Dicas de Exercícios</h3>{' '}
+              </a>
+              <Card.Text>
+                Pratique alongamentos junto de profissionais. Acesse nosso
+                acervo de vídeo aulas.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <br />
+            <Card.Img variant="top" src={food} />
+            <Card.Body>
+              <br />
+              <br />
+              <a href="/">
+                {' '}
+                <h3>Dicas de Alimentação</h3>{' '}
+              </a>
+              <Card.Text>
+                Veja as dicas de outros caminhoneiros dos melhores restaurantes
+                pela estrada.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+        <br />
+        <br />
+        <Footer />
       </div>
     );
   }
