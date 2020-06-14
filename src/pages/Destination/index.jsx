167baea37d1d2e667224b3f65571e6fda27ec6ce
Thiego Carvalho <thiego@rocketmail.com>
2020-06-14 19:06:25 -0300

    Pagina de destino pegando o Lat Long do device

--- src/pages/Destination/index.jsx
+++ src/pages/Destination/index.jsx
@@ -1,61 +1,66 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 import './styles.css';
 import logo from '../../assets/flat.png';
-import { Navbar, Button, Form } from 'react-bootstrap';
+import {Navbar, Button, Form} from 'react-bootstrap';
 
 import Footer from '../../components/Footer';
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div id="home-page">
-        <Navbar id="navbar">
-          <Navbar.Brand>
-            <img
-              src={logo}
-              width="70"
-              height="70"
-              className="d-inline-block align-top"
-              alt="logo cristal"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <p className="welcome">Bem vindo!</p>
-          </Navbar.Collapse>
-        </Navbar>
 
-        <Form>
-          <br />
-          <h2>Vamos começar!</h2>
-          <br />
-          <br />
-          <Form.Group controlId="formGroupEmail">
-            <Form.Control
-              id="input"
-              type="email"
-              placeholder="Digite seu email, telefene ou CPF"
-            />
-          </Form.Group>
-          <Form.Group controlId="formGroupPassword">
-            <Form.Control
-              id="input"
-              type="password"
-              placeholder="Digite sua senha"
-            />
-          </Form.Group>
-          <Button id="btn-login" variant="info">
-            <Link to="/dashboard">Entrar</Link>
-          </Button>
-          <br />
-          <br />
-          <Link to="/newpassword"> Esqueci minha senha</Link>
-        </Form>
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                console.log("Latitude is :", position.coords.latitude);
+                console.log("Longitude is :", position.coords.longitude);
+            },
+            function (error) {
+                console.error("Error Code = " + error.code + " - " + error.message);
+            }
+        );
+    }
 
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="destination-page">
+                <Navbar id="navbar">
+                    <Navbar.Brand>
+                        <img
+                            src={logo}
+                            width="70"
+                            height="70"
+                            className="d-inline-block align-top"
+                            alt="logo cristal"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                    <Navbar.Collapse className="justify-content-end">
+                        <p className="welcome">Para onde vamos?</p>
+                    </Navbar.Collapse>
+                </Navbar>
+
+                <Form>
+                    <br/>
+                    <h2>Digite o seu destino abaixo:</h2>
+                    <br/>
+                    <br/>
+                    <Form.Group controlId="formGroupEmail">
+                        <Form.Control
+                            id="input"
+                            type="address"
+                            placeholder="Endereço do seu destino"
+                        />
+                    </Form.Group>
+                    <Button id="btn-login" variant="info">
+                        <Link to="/dashboard">Enviar</Link>
+                    </Button>
+                    <br/>
+                    <br/>
+                </Form>
+
+                <Footer/>
+            </div>
+        );
+    }
 }
